581c72f6c28c5b910ce6c6faf778379cc6463dd5
David Steinberg <david@resium.com>
2017-04-25 12:02:55 -0700

    Extend ga4gh-base-controller

---
+++ index.js
@@ -0,0 +1,7 @@
+var controller = require('ga4gh-base-controller');
+
+controller.searchExpressionLevels = function(call, callback) {
+  callback(null, {hi: "there"})
+}
+
+module.exports = controller;
